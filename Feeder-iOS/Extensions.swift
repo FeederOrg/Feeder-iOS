044b49d950c62f9dc18a16799f1da36bd3243344
Joe Diragi <joyod3@gmail.com>
2022-09-06 12:31:30 -0400

    Creates Extensions with conditional view modifier

--- Feeder-iOS/Extensions.swift
+++ Feeder-iOS/Extensions.swift
@@ -5,4 +5,30 @@
 //  Created by Joe Diragi on 8/26/22.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
+
+extension Bool {
+     static var iOS16: Bool {
+         guard #available(iOS 16, *) else {
+             // It's iOS 16 so return true.
+             return false
+         }
+         // It isn't, return false
+         return true
+     }
+ }
