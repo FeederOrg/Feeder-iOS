c870e6f7a6511ea9d111bf11186618075e129eb7
Joe Diragi <joyod3@gmail.com>
2022-09-10 20:38:31 -0400

    Sick of it, try and recursively list everything everywhere

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -6,5 +6,16 @@
 #  Created by Joe Diragi on 9/10/22.
 #
 
-echo "======Workspace root======="
-echo $(ls /Volumes/workspace)
+# Brute force
+
+echo "========Recursive root ls========="
+echo $(ls -R /)
+echo "=================================="
+
+echo "======Recursive volumes ls========"
+echo $(ls -R /Volumes/)
+echo "=================================="
+
+echo "======Recursive workspace ls======"
+echo $(ls -R /Volumes/workspace)
+echo "=================================="
