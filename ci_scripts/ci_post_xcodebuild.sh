e890e01daf5d0d5c0b0c586cbb1a4c03b82f19d9
Joe Diragi <joyod3@gmail.com>
2022-09-10 20:27:26 -0400

    Continue trying to find logs before they are uploaded

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -5,14 +5,6 @@
 #
 #  Created by Joe Diragi on 9/10/22.
 #
-test_logs=$CI_DERIVED_DATA_PATH/Logs/Test
-echo $(ls $test_logs)
 
-echo "=============START================"
-for f in "$test_logs"/*
-do
-    echo $(basename $f)
-    echo "-------------------------"
-    echo $(cat $f)
-done
-echo "==============END==============="
+echo "======Workspace root======="
+echo $(ls /Volumes/workspace)
